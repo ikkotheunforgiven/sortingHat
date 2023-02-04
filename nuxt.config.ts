f2b50db5fc1447ab097a993d6aca885c115286fd
ikkotheunforgiven <ikkotheunforgiven@gmail.com>
2023-02-04 20:27:56 +0100

    Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    app: {
+      head: {
+        title: 'Sorting Hat',
+        link: [{ rel: 'icon', type: 'image/png', href: '/img/hogwarts.png' }, { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Life+Savers&display=swap'}]
+      },
+      pageTransition: { name: 'page', mode: 'out-in' }
+    },
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
 })
