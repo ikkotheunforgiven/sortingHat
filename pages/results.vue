f0f52b40a00965d873a8aed442af54d921178680
ikkotheunforgiven <ikkotheunforgiven@gmail.com>
2023-02-06 11:10:50 +0100

    Results page done

---
+++ pages/results.vue
@@ -0,0 +1,15 @@
+<script setup>
+    definePageMeta({
+        pageTransition: {
+            name: 'rotate'
+        }
+    })
+</script>
+
+<template>
+    <div class="container mx-auto pt-24">
+        <img class="mx-auto mb-5 w-1/2" src="/img/gryffindor.png" alt="shield">
+        <h1 class="text-center text-5xl">Congratulations!</h1>
+        <p class="text-center text-lg mt-5">You belong to Gryffindor House.</p>
+    </div>
+</template>
