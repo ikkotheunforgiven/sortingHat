d400ca04d2998fce62f60227ddebd0cd097880c9
ikkotheunforgiven <ikkotheunforgiven@gmail.com>
2023-02-06 02:33:03 +0100

    User messages

---
+++ components/User.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const props = defineProps({
+        answer: String
+    })
+</script>
+
+<template>
+    <div class="flex flex-row-reverse w-1/2 pt-10" v-if="answer != ''">
+        <img src="https://i.pinimg.com/originals/94/e1/73/94e173ce3bf0e8996cd8fa1b30b0fe57.jpg" class="self-end rounded-full w-16 ml-4" alt="user">
+        <div class="flex flex-col">
+            <div class="bg-green-700 rounded-3xl rounded-br-none shadow-sm p-4 mb-2">
+                <p class="text-xl">{{ answer }}</p>
+            </div>
+            <small class="self-end">Wizard</small>
+        </div>
+    </div>
+</template>
